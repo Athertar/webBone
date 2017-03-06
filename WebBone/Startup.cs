46a013699463e9dce355fa9c0a69f29ee1bc1127
Spritzkopf <yannick.weber95@googlemail.com>
2017-03-06 16:48:36 +0100

    Add project files.

---
+++ WebBone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBone.Startup))]
+namespace WebBone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
